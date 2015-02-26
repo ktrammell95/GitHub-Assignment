4f43b573a6b5bc61899a8374b6b7483739927115
Katherine Trammell <ktrammell@gmail.com>
2015-02-26 15:17:06 -0500

    JD helped fix rateload issue

--- js/github.js
+++ js/github.js
@@ -1,14 +1,18 @@
+function icon(name) {
+  return JST.icon({icon: name});
+}
+
 //API pull in that JD created//
 
 var Github = (function() {
 
-  // https://api.github.com/users/jisaacks/repos
-  // https://api.github.com/users/jisaacks
-  // https://api.github.com/users/jisaacks/starred
-  // https://api.github.com/users/jisaacks/orgs
+  // https://api.github.com/users/ktrammell95/repos
+  // https://api.github.com/users/ktrammell95
+  // https://api.github.com/users/ktrammell95/starred
+  // https://api.github.com/users/ktrammell95/orgs
 
-  function Github(user) {
-    this.username = user;
+  function Github(name) {
+    this.username = name;
 
     var apiBase = "https://api.github.com/users/";
 
@@ -23,6 +27,9 @@ var Github = (function() {
   Github.prototype = {
     hitApi: function(url, cb) {
       $.ajax(url, {
+        data: {
+          access_token: "48b50eddb224e5955ee636a2db61bcf72bdc90d2"
+        },
         success: function(data) {
           cb(data);
         },
@@ -50,36 +57,31 @@ var Github = (function() {
 
     loadAll: function(cb) {
       // calls callback with hash of data like below
-      // {
-      //   repos: //repo data,
-      //   user: //user data,
-      //   starred: //starred data,
-      //   orgs: //org data
-      // }
+ 
       var dataGroups = {};
 
       var afterCB = _.after(4, cb);
 
       // 1 getting repos
-      github.repos(function(data){
+      this.repos(function(data){
         dataGroups.repos = data;
         afterCB(dataGroups);
       });
 
       // 2 getting orgs
-      github.orgs(function(data){
+      this.orgs(function(data){
         dataGroups.orgs = data;
         afterCB(dataGroups);
       });
 
       // 3 getting user
-      github.user(function(data){
+      this.user(function(data){
         dataGroups.user = data;
         afterCB(dataGroups);
       });
 
       // 4 getting starred
-      github.starred(function(data){
+      this.starred(function(data){
         dataGroups.starred = data;
         afterCB(dataGroups);
       });
